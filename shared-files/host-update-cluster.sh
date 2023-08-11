b0bd442245722249c689603aa4d570a9452be87a
georemo <george.oremo@gmail.com>
2023-08-11 19:43:47 +0300

    -

--- shared-files/host-update-cluster.sh
+++ shared-files/host-update-cluster.sh
@@ -8,7 +8,7 @@ export CLUSTER_MEMBER="routed-93"
 export EXEC_FILE="host-update-cluster.sh"
 export SHARED_FILES_HOST="/home/${HOST_USER}/cb-bootstrap/shared-files"
 export SHARED_FILES_CLUSTER_MEMBER="/home/${CB_OPERATOR}/cb-bootstrap/shared-files"
-export FX_DIR="${SHARED_FILES_CLUSTER_MEMBER}/fx.sh"
+export FX_DIR="${SHARED_FILES_HOST}/fx.sh"
 
 
 # print header
