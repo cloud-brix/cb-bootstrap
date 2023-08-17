5cbc91a97aa2d3f19eeaeb8789535bf76ec8027a
georemo <george.oremo@gmail.com>
2023-08-17 10:41:37 +0300

    -

--- shared-files/installer-nodejs.sh
+++ shared-files/installer-nodejs.sh
@@ -6,16 +6,7 @@ pwd
 echo "--------confirm current current user:"
 whoami
 echo "--------installing nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash'
-
-sudo -H -u devops bash -c '
-echo "--------restarting user session for specific node installation"
-#su devops
-echo "--------changing to home"
-cd /home/devops
-echo "--------confirm current directory:"
-pwd
-echo "--------confirm current current user:"
-whoami
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+echo "--------su devops"
 echo "--------installing node v16.20.1"
 nvm install v16.20.1'
